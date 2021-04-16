c071640a7f00c9d13edbd9a99604e2480148b82c
Songxuan Li <lisongxuan0214@gmail.com>
2021-04-16 13:50:41 +0800

    functional test now checks we can input a to-do iten

--- functional_tests.py
+++ functional_tests.py
@@ -5,9 +5,27 @@ class NewVisitorTest(unittest.TestCase):
         self.browser = webdriver.Chrome(executable_path=r'C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe')
     def tearDown(self):
         self.browser.quit()
-    def test(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do',self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'enter a to-do item'
+            )
+
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_test_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1.Buy peacock feathers' for row in rows)
+            )
+        
         self.fail('finish the test')
 
 if __name__ ==  '__main__':
